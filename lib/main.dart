c962c1ce470b249519d6a0e95f9333744d6d7cd5
Shaharyar417 <shaharyar.bcss19@iba-suk.edu.pk>
2022-12-30 03:39:08 +0500

    carousel removed 1

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:mad_project/pages/userdetails.dart';
 import 'package:mad_project/users/lessor.dart';
 import 'package:mad_project/widgets/MyDrawerHeader.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:mad_project/widgets/carousal.dart';
 import 'helper/utils.dart';
 
 Future<void> main() async {

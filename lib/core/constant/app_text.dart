82ab04c63e123acf5b649260fa611b585f8263f4
syedhurera <abuhurerasyed223@gmail.com>
2022-12-31 02:08:46 +0500

    adding login and signup

---
+++ lib/core/constant/app_text.dart
@@ -0,0 +1,19 @@
+class AppText {
+  static const String login = "Login";
+  static const String signUp = "Sign Up";
+  static const String email = "Email";
+  static const String password = "Password";
+  static const String firstName = "Name";
+  static const String contact = "Contact";
+  static const String cnic = "CNIC";
+  static const String city = "City";
+  static const String address = "Address";
+  static const String confirm = "Confirm Password";
+  static const String rememberMe = "Remember Me";
+  static const String forgotPassword = "Forgot Password";
+  static const String account = "Don't have an account?";
+  static const String already = "Already have an account?";
+  static const String registernow = "Register Now";
+  static const String loginwith = "Log in with";
+  static const String or = "OR";
+}
